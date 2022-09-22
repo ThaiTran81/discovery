250d3748ddb19740fd150ace176ee7c9945d22ec
Tran Hoang Thai <thai.tranhoang@elca.vn>
2022-09-22 10:52:45 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ *
+ * @author ThaiTran81
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
